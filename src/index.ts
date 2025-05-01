b821fa3c70409217b77a4cf858459ccceeaaa7d8
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-05-02 03:04:37 +0900

    refactor: use namespaced export instead of exporting object for `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
 // SPDX-License-Identifier: MPL-2.0
 
-import type Distree from "./Distree.ts"
-import ancestors from "./ancestors.ts"
-import from from "./from.ts"
-import fromDirectory from "./fromDirectory.ts"
-import has from "./has.ts"
-import insert from "./insert.ts"
-import isDistree from "./isDistree.ts"
-import locate from "./locate.ts"
-import remove from "./remove.ts"
-import transform from "./transform.ts"
-import transformAsync from "./transformAsync.ts"
+/* eslint-disable consistent-default-export-name/default-export-match-filename */
 
-type index<T> = Distree<T>
-const index = Object.freeze({
-	ancestors,
-	from,
-	fromDirectory,
-	has,
-	insert,
-	isDistree,
-	locate,
-	remove,
-	transform,
-	transformAsync,
-})
+import type _Distree from "./Distree.ts"
+import _ancestors from "./ancestors.ts"
+import _from from "./from.ts"
+import _fromDirectory from "./fromDirectory.ts"
+import _has from "./has.ts"
+import _insert from "./insert.ts"
+import _isDistree from "./isDistree.ts"
+import _locate from "./locate.ts"
+import _remove from "./remove.ts"
+import _transform from "./transform.ts"
+import _transformAsync from "./transformAsync.ts"
 
-export default index
+namespace Distree {
+	export const ancestors = _ancestors
+	export const from = _from
+	export const fromDirectory = _fromDirectory
+	export const has = _has
+	export const insert = _insert
+	export const isDistree = _isDistree
+	export const locate = _locate
+	export const remove = _remove
+	export const transform = _transform
+	export const transformAsync = _transformAsync
+}
+
+type Distree<T> = _Distree<T>
+
+export default Distree
